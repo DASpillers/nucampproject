a6ea38d1cb29271d9908015fefda0d0c51b03b72
Daniel Spillers <spillersdaniela@gmail.com>
2021-07-26 19:13:42 -0400

    NPM scripts part 1: moved js to external js and added onchange and parashell

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function () {
+    $(".carousel").carousel({ interval: 2000 }); //sets carousel to change between slides at 2 seconds/2000ms
+    $("#carouselButton").click(function () { //defines what happens when you click #carouselButton
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) { //if the carouselButton has a fa-pause class...
+            $(".carousel").carousel("pause"); //Pause the Carousel
+            $("#carouselButton").children("i").removeClass("fa-pause"); //Then take the fa-pause class out
+            $("#carouselButton").children("i").addClass("fa-play"); //replace it with an fa-play class
+        } else { //if not
+            $(".carousel").carousel("cycle"); //cycle through the carousel
+            $("#carouselButton").children("i").removeClass("fa-play"); //take the fa-play class out
+            $("#carouselButton").children("i").addClass("fa-pause"); //replace it with an fa-pause class
+        }
+    });
+});
